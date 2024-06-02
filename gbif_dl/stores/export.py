b06e41f0336ea34298602c61da9e097f7966da8a
Nathan Oasis <puddleoasis@gmail.com>
2024-06-02 00:32:10 -0400

    Update export.py
    changing `fieldnames` to save as an parameter.

--- gbif_dl/stores/export.py
+++ gbif_dl/stores/export.py
@@ -2,9 +2,9 @@ import csv
 from . import MediaData
 
 
-def to_csv(generator, filename):
+def to_csv(generator, filename, fieldnames):
     with open(filename, "w", newline="") as csvfile:
-        fieldnames = list(MediaData.__required_keys__)
+        # fieldnames = list(MediaData.__required_keys__)
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
         for data in generator:
